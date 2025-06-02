1e18df22c95f9641d65e4f774e9f70a4cf77e56d
Abbonian <jirawatee@gmail.com>
2025-06-02 14:14:26 +0700

    Change GEMINI_API_KEY to API_KEY

--- functions/utils/gemini.js
+++ functions/utils/gemini.js
@@ -1,5 +1,5 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 class Gemini {
   async multimodal(promptArray) {
